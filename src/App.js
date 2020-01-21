f4112efb734d6687c314e7622821f67f4dd6c0d0
陳冠哲 <jeremy.chen@newpos.tw>
2020-01-21 21:23:38 +0800

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 import './App.css'
 
@@ -7,7 +8,7 @@ import HomePage from './pages/homepage/homepage.component'
 const App = () => {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
     </div>
   )
 }
