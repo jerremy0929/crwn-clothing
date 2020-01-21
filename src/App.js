f5e0c0b753cad90fe4465e40fce02e2529869da0
陳冠哲 <jeremy.chen@newpos.tw>
2020-01-21 10:23:19 +0800

    created project with create react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import HomePage from './components/homepage.component'
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
